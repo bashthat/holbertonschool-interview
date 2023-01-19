0b895a581533b887593f8f94e67bc6ad65d00fd5
SamAlberts <97474996+bashthat@users.noreply.github.com>
2023-01-19 23:50:32 +0000

    module

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-
+'''Returns true or false depending on keys and matching boxes'''
 def canUnlockAll(boxes)
-
-
+# array for boxes
 keys = [0]
-
-
+# searching through boxes
 for x in keys:
   for k in boxes[x]:
     if k not in keys and k < len(boxes):
       keys.append(k)
-
-
-
+# else if return False
 xyz = 0
 while xyz < len(boxes):
   if xyz not in keys:
     return False
 
-    xyz += 1
-
+  xyz += 1
 
 return True

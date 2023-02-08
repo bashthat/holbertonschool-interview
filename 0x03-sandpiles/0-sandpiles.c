1e988f3ce1c3339e450b5b5df7d519e89c25692e
bashthat <3249@holbertonstudents.com>
2023-02-08 17:19:59 -0600

    sandpiles

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -4,12 +4,43 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/**
+ * print_grid_sum - Print 3x3 grids sum
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ */
+static void print_grid_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid1[i][j]);
+        }
+
+        printf(" %c ", (i == 1 ? '+' : ' '));
+
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid2[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 /**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
- * Return: NULL
-*/
-
+ *
+ */
 static void print_grid(int grid[3][3])
 {
     int i, j;
@@ -26,19 +57,43 @@ static void print_grid(int grid[3][3])
     }
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+    int grid1[3][3] = {
+        {3, 3, 3},
+        {3, 3, 3},
+        {3, 3, 3}
+    };
+    int grid2[3][3] = {
+        {1, 3, 1},
+        {3, 3, 3},
+        {1, 3, 1}
+    };
+
+    print_grid_sum(grid1, grid2);
+
+    sandpiles_sum(grid1, grid2);
+
+    printf("=\n");
+    print_grid(grid1);
+
+    return (EXIT_SUCCESS);
+}
 
 /**
- * sandpiles_sum - sandpiling grids
- * @grid1: prints every topppling round
- * @grid2: second grid for sandpile
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
  * Return: NULL
- * 
 */
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    
-    
-    int i, j, unstable = 0;
+    int i, j;
 
     for (i = 0; i < 3; i++)
     {
@@ -47,43 +102,5 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
             grid1[i][j] += grid2[i][j];
         }
     }
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (grid1[i][j] > 3)
-            {
-                unstable = 1;
-                break;
-            }
-        }
-    }
-
-    if (unstable == 1)
-    {
-        printf("=\n");
-        print_grid(grid1);
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] > 3)
-                {
-                    grid1[i][j] -= 4;
-                    if (i - 1 >= 0)
-                        grid1[i - 1][j] += 1;
-                    if (i + 1 < 3)
-                        grid1[i + 1][j] += 1;
-                    if (j - 1 >= 0)
-                        grid1[i][j - 1] += 1;
-                    if (j + 1 < 3)
-                        grid1[i][j + 1] += 1;
-                }
-            }
-        }
-        sandpiles_sum(grid1, grid2);
-    }
-   
-
+    topple(grid1);
 }

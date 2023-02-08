6c69aba6ba90317c828800e7abe6fd713ac506ab
bashthat <3249@holbertonstudents.com>
2023-02-08 17:30:18 -0600

    sandpiles edited

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -1,70 +1,60 @@
 #include "sandpiles.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-/**
- * topple - topple sandpiles
- * @grid: 3x3 grid
- * Return: NULL
-*/
 
-void topple(int grid[3][3])
-{
-    int i, j, stable = 0;
-    int grid2[3][3];
 
-    while (stable == 0)
-    {
-        stable = 1;
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                grid2[i][j] = grid[i][j];
-            }
-        }
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid2[i][j] > 3)
-                {
-                    stable = 0;
-                    grid[i][j] -= 4;
-                    if (i - 1 >= 0)
-                        grid[i - 1][j] += 1;
-                    if (i + 1 < 3)
-                        grid[i + 1][j] += 1;
-                    if (j - 1 >= 0)
-                        grid[i][j - 1] += 1;
-                    if (j + 1 < 3)
-                        grid[i][j + 1] += 1;
-                }
-            }
-        }
-        if (stable == 0)
-            print_grid(grid);
+void print_grid(int grid[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      printf("%d ", grid[i][j]);
     }
+    printf("\n");
+  }
 }
 
-/**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- * Return: NULL
-*/
-
-void sandpiles_sum(int grid1[3][3], int grid2[3][3])
-{
-    int i, j;
-
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
+void topple(int grid[3][3]) {
+  int copy[3][3];
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      copy[i][j] = grid[i][j];
     }
+  }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (grid[i][j] > 3) {
+        copy[i][j] -= 4;
+        if (i + 1 < 3) copy[i + 1][j] += 1;
+        if (j + 1 < 3) copy[i][j + 1] += 1;
+        if (i - 1 >= 0) copy[i - 1][j] += 1;
+        if (j - 1 >= 0) copy[i][j - 1] += 1;
+      }
+    }
+  }
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      grid[i][j] = copy[i][j];
+    }
+  }
+}
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      grid1[i][j] += grid2[i][j];
+    }
+  }
+  while (1) {
+    int unstable = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (grid1[i][j] > 3) {
+          unstable = 1;
+          break;
+        }
+      }
+    }
+    if (!unstable) break;
+    printf("=\n");
+    print_grid(grid1);
     topple(grid1);
+  }
 }
--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -1,6 +1,12 @@
 #ifndef SAND_H
 #define SAND_H
 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void topple(int grid[3][3]);
 int is_stable(int grid[3][3]);

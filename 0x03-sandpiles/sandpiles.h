12aa04331356abb7b44c0dc9d2e04b67f862faa4
bashthat <3249@holbertonstudents.com>
2023-02-08 17:23:37 -0600

    updating

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -1,8 +1,6 @@
 #ifndef SAND_H
 #define SAND_H
 
-static void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 void topple(int grid[3][3]);
 int is_stable(int grid[3][3]);

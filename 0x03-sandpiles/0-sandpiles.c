847938778d83102a03e647cacf34084c02eabce0
bashthat <3249@holbertonstudents.com>
2023-02-08 21:46:27 -0600

    editing

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -12,6 +12,7 @@ for (i = 0; i < 3; i++)
 for (j = 0; j < 3; j++)
 {
 if (j) printf(" ");
+
 printf("%d", grid[i][j]);
 }
 printf("\n");
@@ -45,6 +46,7 @@ for (i = 0; i < 3; i++)
 {
 for (j = 0; j < 3; j++)
 {
+
 if (grid[i][j] > 3) return (0);
 }
 }
@@ -58,8 +60,10 @@ return (1);
 void copy_grid(int grid1[3][3], int grid2[3][3])
 {
 int i, j;
+
 for (i = 0; i < 3; i++)
 {
+
 for (j = 0; j < 3; j++)
 {
 grid1[i][j] = grid2[i][j];
@@ -75,16 +79,24 @@ void topple(int grid[3][3])
 int i, j;
 int grid_copy[3][3];
 copy_grid(grid_copy, grid);
+
 for (i = 0; i < 3; i++)
 {
+
 for (j = 0; j < 3; j++)
 {
+
 if (grid_copy[i][j] > 3)
 {
+
 grid[i][j] -= 4;
+
 if (i - 1 >= 0) grid[i - 1][j]++;
+
 if (i + 1 < 3) grid[i + 1][j]++;
+
 if (j - 1 >= 0) grid[i][j - 1]++;
+
 if (j + 1 < 3) grid[i][j + 1]++;
 }
 }

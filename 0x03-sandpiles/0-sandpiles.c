c626f5a6ccf66fd0e6760e7a398eb3089e8ee765
bashthat <3249@holbertonstudents.com>
2023-02-08 21:18:11 -0600

    updatingScripts

---
+++ 0x03-sandpiles/0-sandpiles.c
@@ -0,0 +1,107 @@
+#include "sandpiles.h"
+
+/**
+ * print_grid - prints a 3x3 grid
+ * @grid: the grid to print
+ */
+
+void print_grid(int grid[3][3])
+{
+    int i, j;
+
+    printf("=\n");
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void compute_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            grid1[i][j] += grid2[i][j];
+        }
+    }
+}
+
+/**
+ * is_stable - checks if a grid is stable
+ * @grid: the grid to check
+ * Return: 1 if stable, 0 otherwise
+ */
+
+int is_stable(int grid[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid[i][j] > 3)
+                return (0);
+        }
+    }
+    return (1);
+}
+
+/**
+ * topple - topples a grid
+ * @grid: the grid to topple
+ */
+
+void topple(int grid[3][3])
+{
+    int i, j;
+    int grid_copy[3][3];
+
+    copy_grid(grid_copy, grid);
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (grid_copy[i][j] > 3)
+            {
+                grid[i][j] -= 4;
+                if (i - 1 >= 0)
+                    grid[i - 1][j]++;
+                if (i + 1 < 3)
+                    grid[i + 1][j]++;
+                if (j - 1 >= 0)
+                    grid[i][j - 1]++;
+                if (j + 1 < 3)
+                    grid[i][j + 1]++;
+            }
+        }
+    }
+}
+
+/**
+ * sandpiles_sum - computes the sum of two sandpiles
+ * @grid1: the first grid
+ * @grid2: the second grid
+ */
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    compute_sum(grid1, grid2);
+
+    while (!is_stable(grid1))
+    {
+        print_grid(grid1);
+        topple(grid1);
+    }
+}
--- 0x03-sandpiles/README.md
+++ 0x03-sandpiles/README.md
@@ -8,3 +8,22 @@
 https://stackoverflow.com/questions/63420373/what-are-the-characteristics-of-an-abelian-sandpiles-for-a-grid-3-x-3
 
 https://www.youtube.com/watch?v=diGjw5tghYU
+
+
+ the sandpile array is declared as a global variable, which eliminates the need to use malloc to dynamically allocate memory. The add_sand function implements the rules for adding sand to the pile and collapsing the pile when it becomes unstable.
+ the compute_sandpiles function takes two sandpile arrays as arguments and computes the next state of the sandpile by adding the contents of one pile to the other. The function uses the add_sand function to add sand to the pile and collapse the pile when it becomes unstable.
+ =============================================
+The process of solving a sandpile algorithm involves simulating the behavior of sand piles, where each cell in a grid can hold a certain number of sand grains. The goal is to create a stable sandpile, where each cell has a maximum of three grains of sand.
+
+Here is a high-level overview of the process:
+
+Initialize the grid: Set the number of sand grains in each cell to a specified value.
+
+Check for instability: For each cell, if it has four or more grains of sand, it is considered unstable and a grain must be distributed to its four neighboring cells.
+
+Distribute sand: For each unstable cell, distribute one grain of sand to each of its four neighboring cells.
+
+Repeat steps 2 and 3 until all cells are stable: Keep repeating the check for instability and distribution of sand until all cells have three or fewer grains of sand.
+
+Stable sandpile: When all cells are stable, the sandpile is considered to be in its final state.
+=============================================================

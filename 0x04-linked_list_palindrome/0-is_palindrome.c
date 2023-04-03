d593a8549ecdf30fb2ec898d3090539306f603b8
bashthat <earthstar412@gmail.com>
2023-04-03 16:59:55 -0500

    updating

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -2,43 +2,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+/**
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: pointer to head of list
+ * Return: number of elements in list
+ */
+int listint_len(const listint_t *h)
+{
+    int i = 0;
+
+    while (h)
+    {
+        i++;
+        h = h->next;
+    }
+    return (i);
+}
+
+/**
+ * backwards - checks if a singly linked list is reversed
+ * @head: double pointer to head of list
+ * @len: length of list
+ * Return: 0 if it is not reversed, 1 if it is reversed
+ */
+int backwards(listint_t **head, int len)
+{
+    int i = 0;
+    listint_t *temp = *head;
+
+    while (i < len / 2)
+    {
+        temp = temp->next;
+        i++;
+    }
+    if (len % 2 != 0)
+        temp = temp->next;
+    i = 0;
+    while (i < len / 2)
+    {
+        if (temp->n != (*head)->n)
+            return (0);
+        temp = temp->next;
+        *head = (*head)->next;
+        i++;
+    }
+    return (1);
+}
+
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: double pointer to head of list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
-    listint_t *current;
-    int i, j, len, *arr;
+    int len = 0;
 
-    if (head == NULL || *head == NULL)
+    if (!head || !*head)
         return (1);
-    current = *head;
-    len = 0;
-    while (current != NULL)
-    {
-        len++;
-        current = current->next;
-    }
-    arr = malloc(sizeof(int) * len);
-    if (arr == NULL)
-        return (0);
-    current = *head;
-    for (i = 0; i < len; i++)
-    {
-        arr[i] = current->n;
-        current = current->next;
-    }
-    for (i = 0, j = len - 1; i < j; i++, j--)
-    {
-        if (arr[i] != arr[j])
-        {
-            free(arr);
-            return (0);
-        }
-    }
-    free(arr);
-    return (1);
+    len = listint_len(*head);
+    return (backwards(head, len));
+
 }

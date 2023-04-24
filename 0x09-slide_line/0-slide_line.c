ecf296cd0eebb201a94536c9a25f3c67739fa64f
bashthat <earthstar412@gmail.com>
2023-04-24 07:53:38 -0500

    updinging

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -1,4 +1,7 @@
 #include "slide_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
  * slide_line - slides and merges an array of integers
@@ -9,3 +12,91 @@
  */
 
 int slide_line(int *line, size_t size, int direction)
+{
+int x, y;
+if (direction == SLIDE_LEFT)
+{
+for (x = 0; x < (int)size; x++)
+{
+if (line[x] == 0)
+{
+for (y = x + 1; y < (int)size; y++)
+{
+if (line[y] != 0)
+{
+line[x] = line[y];
+line[y] = 0;
+break;
+}
+}
+}
+}
+for (x = 0; x < (int)size - 1; x++)
+{
+if (line[x] == line[x + 1])
+{
+line[x] = line[x] + line[x + 1];
+line[x + 1] = 0;
+}
+}
+for (x = 0; x < (int)size; x++)
+{
+if (line[x] == 0)
+{
+for (y = x + 1; y < (int)size; y++)
+{
+if (line[y] != 0)
+{
+line[x] = line[y];
+line[y] = 0;
+break;
+}
+}
+}
+}
+return (1);
+}
+else if (direction == SLIDE_RIGHT)
+{
+for (x = (int)size - 1; x >= 0; x--)
+{
+if (line[x] == 0)
+{
+for (y = x - 1; y >= 0; y--)
+{
+if (line[y] != 0)
+{
+line[x] = line[y];
+line[y] = 0;
+break;
+}
+}
+}
+}
+for (x = (int)size - 1; x > 0; x--)
+{
+if (line[x] == line[x - 1])
+{
+line[x] = line[x] + line[x - 1];
+line[x - 1] = 0;
+}
+}
+for (x = (int)size - 1; x >= 0; x--)
+{
+if (line[x] == 0)
+{
+for (y = x - 1; y >= 0; y--)
+{
+if (line[y] != 0)
+{
+line[x] = line[y];
+line;
+}
+}
+}
+}
+return (1);
+}
+return (0);
+}
+// Path: holbertonschool-interview\0x09-slide_line\slide_line.h
--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -3,4 +3,7 @@
 
 int slide_line(int *line, size_t size, int direction);
 
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT -1
+
 #endif /*SLIDE_H*/

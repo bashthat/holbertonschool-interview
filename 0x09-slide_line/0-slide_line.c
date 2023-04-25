daaa01f712c9e908ccd929ae53669e6c843a82a9
bashthat <earthstar412@gmail.com>
2023-04-24 23:11:04 -0500

    addingSlideLine

--- 0x09-slide_line/0-slide_line.c
+++ 0x09-slide_line/0-slide_line.c
@@ -1,84 +1,82 @@
 #include "slide_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
  * slide_line - slides and merges an array of integers
- * @line: points to an array of integers
- * @size: number of elements in array
+ * @line: array of integers
+ * @size: size of array
  * @direction: direction to slide and merge
- * Return: 1 upon success, or 0 upon failure
+ * Return: 1 on success, 0 on failure
  */
+
 int slide_line(int *line, size_t size, int direction)
 {
-if (direction == SLIDE_LEFT)
-slide_left(line, size);
-else if (direction == SLIDE_RIGHT)
-slide_right(line, size);
-else
-return (0);
-return (1);
+    if (!line || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+        return (0);
+    if (direction == SLIDE_LEFT)
+        slide_left(line, size);
+    else
+        slide_right(line, size);
+    return (1);
 }
 
 /**
  * slide_left - slides and merges an array of integers to the left
- * @line: points to an array of integers
- * @size: number of elements in array
+ * @line: array of integers
+ * @size: size of array
  */
 
 void slide_left(int *line, size_t size)
 {
-int i, j;
-for (i = 0; i < (int)size; i++)
-{
-if (line[i] == 0)
-{
-for (j = i + 1; j < (int)size; j++)
-{
-if (line[j] != 0)
-{
-line[i] = line[j];
-line[j] = 0;
-break;
-}
-}
-}
-}
-for (i = 0; i < (int)size; i++)
-{
-if (line[i] == line[i + 1])
-{
-line[i] *= 2;
-line[i + 1] = 0;
-}
-}
+    int x = 0, y = 1, temp = 0;
+
+    for (; y < (int)size; y++)
+    {
+        if (line[y] == 0)
+            continue;
+        temp = line[x];
+        if (temp == line[y])
+        {
+            line[x++] = temp + line[y];
+            line[y] = 0;
+        }
+        else if (temp == 0)
+        {
+            line[x] = line[y];
+            line[y] = 0;
+        }
+        else
+            line[++x] = line[y];
+    }
 /**
  * slide_right - slides and merges an array of integers to the right
- * @line: points to an array of integers
- * @size: number of elements in array
- */
+ * @line: array of integers
+ * @size: size of array
+*/
 void slide_right(int *line, size_t size)
 {
-for (i = 0; i < (int)size; i++)
-{
-if (line[i] == 0)
-{
-for (j = i + 1; j < (int)size; j++)
-{
-if (line[j] != 0)
-{
-line[i] = line[j];
-line[j] = 0;
-break;
-}
-}
-}
-}
-for (i = (int)size - 1; i >= 0; i--)
-{
-if (line[i] == line[i - 1])
-{
-line[i] *= 2;
-line[i - 1] = 0;
-}
-}
+    int x = size - 1, y = size - 2, temp = 0;
+
+    for (; y >= 0; y--)
+    {
+        if (line[y] == 0)
+            continue;
+        temp = line[x];
+        if (temp == line[y])
+        {
+            line[x--] = temp + line[y];
+            line[y] = 0;
+        }
+        else if (temp == 0)
+        {
+            line[x] = line[y];
+            line[y] = 0;
+        }
+        else
+            line[--x] = line[y];
+    }
 }
 }
--- 0x09-slide_line/slide_line.h
+++ 0x09-slide_line/slide_line.h
@@ -5,10 +5,9 @@
 #include <stddef.h>
 #define SLIDE_LEFT 0
 #define SLIDE_RIGHT 1
-
+/*functions*/
 int slide_line(int *line, size_t size, int direction);
 void slide_left(int *line, size_t size);
 void slide_right(int *line, size_t size);
-
-
+/*endif*/
 #endif
